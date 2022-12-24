0706aafa12b978eab6d16a96b836afc90c75f770
Misteregis <misteregis@gmail.com>
2022-12-24 19:17:25 -0300

    Adicionado Swegger e EF

--- Program.cs
+++ Program.cs
@@ -1,6 +1,48 @@
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Payment API",
+        Description = "Projeto para a realização de teste técnico da DIO.",
+        Contact = new OpenApiContact
+        {
+            Name = "Link do projeto original",
+            Url = new Uri("https://gitlab.com/Pottencial/tech-test-payment-api")
+        }
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configura o pipeline de solicitação.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
