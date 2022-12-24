5c81aa9fc42ef1331bc841c4e9602d756262d309
Misteregis <misteregis@gmail.com>
2022-12-24 19:56:55 -0300

    Criado as classes Product, Seller e EnumSaleStatus

---
+++ Models/EnumSaleStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace PaymentAPI.Models
+{
+    using System.Runtime.Serialization;
+
+    public enum EnumSaleStatus
+    {
+        [EnumMember(Value = "Aguardando pagamento")]
+        PendingPayment,
+        [EnumMember(Value = "Pagamento aprovado")]
+        ApprovedPayment,
+        [EnumMember(Value = "Enviado para transportadora")]
+        SentToCarrier,
+        [EnumMember(Value = "Entregue")]
+        Delivered,
+        [EnumMember(Value = "Cancelada")]
+        Canceled
+    }
+}
