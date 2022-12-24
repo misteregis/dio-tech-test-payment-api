cabb9199f67e78b27a1dda20ebaf87615559249b
Misteregis <misteregis@gmail.com>
2022-12-24 20:46:33 -0300

    Criado a classe Sale

---
+++ Models/Sale.cs
@@ -0,0 +1,28 @@
+﻿namespace PaymentAPI.Models
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        
+        public EnumSaleStatus Status { get; set; }
+
+        public DateTime SaleDate { get; set; }
+
+        public Seller Seller { get; set; }
+        
+        public List<Product> Products { get; set; } = new();
+
+        public Sale(int id, Seller seller)
+        {
+            Id = id;
+            SaleDate = DateTime.Now;
+            Seller = seller;
+            Status = EnumSaleStatus.PendingPayment;
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
