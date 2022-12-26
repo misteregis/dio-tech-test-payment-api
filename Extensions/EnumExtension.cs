f0b25820f5e0c333c7b6a5a4071a81951d6dc1da
Misteregis <misteregis@gmail.com>
2022-12-26 20:09:07 -0300

    Criado extensão para Enum

---
+++ Extensions/EnumExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace PaymentAPI.Extensions;
+
+public static class Extension
+{
+    public static string GetEnumMemberValue(this Enum @enum)
+    {
+        var attr = @enum.GetType().GetMember(@enum.ToString()).FirstOrDefault()?.
+                GetCustomAttributes(false).OfType<EnumMemberAttribute>().
+                FirstOrDefault();
+
+        return attr == null ? @enum.ToString() : attr.Value;
+    }
+}
