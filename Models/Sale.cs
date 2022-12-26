5b6ff491ca91230d7fbe44df763946340e1b5e3b
Misteregis <misteregis@gmail.com>
2022-12-26 18:47:36 -0300

    Adicionado construtor ao Seller e JsonConverter

--- Models/Sale.cs
+++ Models/Sale.cs
@@ -1,15 +1,19 @@
 ﻿namespace PaymentAPI.Models
 {
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
     public class Sale
     {
         public int Id { get; set; }
-        
+
+        [JsonConverter(typeof(StringEnumConverter))]
         public EnumSaleStatus Status { get; set; }
 
         public DateTime SaleDate { get; set; }
 
         public Seller Seller { get; set; }
-        
+
         public List<Product> Products { get; set; } = new();
 
         public Sale(int id, Seller seller)
--- Models/Seller.cs
+++ Models/Seller.cs
@@ -11,14 +11,5 @@
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
-
-        public Seller(int id, string cpf, string name, string email, string phoneNumber)
-        {
-            Id = id;
-            Cpf = cpf;
-            Name = name;
-            Email = email;
-            PhoneNumber = phoneNumber;
-        }
     }
 }
