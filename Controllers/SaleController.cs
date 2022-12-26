89868ea421bd90499b19f6cd2d5af17dd423cc8a
Misteregis <misteregis@gmail.com>
2022-12-26 00:47:08 -0300

    Criado o controller SaleController

---
+++ Controllers/SaleController.cs
@@ -0,0 +1,23 @@
+﻿namespace PaymentAPI.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Options;
+    using Models;
+    using Services;
+    using System.ComponentModel.DataAnnotations;
+
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class SaleController : ControllerBase
+    {
+        private readonly SaleService _saleService;
+        private readonly SellerService _sellerService;
+
+        public SaleController(IOptions<SellerService> selleServiceOptions, IOptions<SaleService> saleServiceOptions)
+        {
+            _sellerService = selleServiceOptions.Value;
+            _saleService = saleServiceOptions.Value;
+        }
+    }
+}
